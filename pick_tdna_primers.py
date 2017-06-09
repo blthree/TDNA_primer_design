65fc8e35525a883c8776db4c5a8e676e3f09dbce
blthree <blthree89@gmail.com>
2017-06-09 19:01:22 -0400

    added PRIMER_PRODUCT_SIZE_RANGE since default range is too short

--- pick_tdna_primers.py
+++ pick_tdna_primers.py
@@ -2,6 +2,7 @@ import click
 from pyfaidx import Fasta
 from primer3.bindings import designPrimers
 
+
 def show_help_if_missing(ctx, param, value):
     if not value:
         click.secho("The '{0}' parameter is mandatory, please check the usage below:\n".format(param.name),
@@ -10,19 +11,23 @@ def show_help_if_missing(ctx, param, value):
         ctx.exit()
     return value
 
+
 @click.command()
 @click.option('-i', '--input_stock_num', help='Stock number to generate primers for', callback=show_help_if_missing)
-@click.option('-N','--maxN', default=300, help='maxN or maximum distance from flanking sequence to T-DNA insertion')
+@click.option('-N', '--maxN', default=300, help='maxN or maximum distance from flanking sequence to T-DNA insertion')
 @click.option('--ext5', default=300, help='ext5, basepairs upstream of T-DNA to exclude')
 @click.option('--ext3', default=300, help='ext3, basepairs downstream of T-DNA to exclude')
 @click.option('-z', '--p_zone', default=200, help='p_zone, size of region for picking each primer')
 @click.option('-s', '--show-seq', default=False, help='Show the sequence around the insertion site', is_flag=True)
 def run_tdna_primers(input_stock_num, maxn, ext5, ext3, p_zone, show_seq):
+
+    # TODO: add option to show primer3 output details and save to file
+
     sequence_length_defs = {
         'maxN': maxn,
         'ext5': ext5,
         'ext3': ext3,
-        'p_zone': p_zone*2  # size of left and right regions together, so twice the Signal.salk.edu value
+        'p_zone': p_zone * 2  # size of left and right regions together, so twice the Signal.salk.edu value
     }
 
     data_filenames = ['data/T-DNA.SALK', 'data/T-DNA.SAIL', 'data/T-DNA.GABI']
@@ -40,9 +45,10 @@ def run_tdna_primers(input_stock_num, maxn, ext5, ext3, p_zone, show_seq):
         result['poly_name'] = poly_name
         p3s, p3p = load_conf()
         primer_results = make_primers(sequence, sequence_length_defs, p3s, p3p)
-        print('LP: ' + primer_results['PRIMER_LEFT_0_SEQUENCE'] + ' Tm: ' + str(round(primer_results['PRIMER_LEFT_0_TM'], 1)))
-        #print('\n')
-        print('RP: ' + primer_results['PRIMER_RIGHT_0_SEQUENCE'] + ' Tm: ' + str(round(primer_results['PRIMER_RIGHT_0_TM'], 1)))
+        print('LP: ' + primer_results['PRIMER_LEFT_0_SEQUENCE'] + ' Tm: ' + str(
+            round(primer_results['PRIMER_LEFT_0_TM'], 1)))
+        print('RP: ' + primer_results['PRIMER_RIGHT_0_SEQUENCE'] + ' Tm: ' + str(
+            round(primer_results['PRIMER_RIGHT_0_TM'], 1)))
         print('\n')
 
 
@@ -53,12 +59,14 @@ def is_int(s):
     except ValueError:
         return False
 
+
 def is_float(s):
     try:
         float(s)
         return True
     except ValueError:
         return False
+
 
 def init_db(filenames):
     assert type(filenames) == list, "Filenames must be in the form of a list"
@@ -77,8 +85,9 @@ def init_db(filenames):
             orientation = s_line[3].split('/')[0]
             # load into dict of dicts object
             if stock_name in records and poly_name in records[stock_name]:
-                records[stock_name][poly_name + '.1'] = {'chr': poly_chr, 'orientation': orientation, 'start': poly_start,
-                                                  'end': poly_end}
+                records[stock_name][poly_name + '.1'] = {'chr': poly_chr, 'orientation': orientation,
+                                                         'start': poly_start,
+                                                         'end': poly_end}
             elif stock_name not in records:
                 records[stock_name] = {
                     poly_name: {'chr': poly_chr, 'orientation': orientation, 'start': poly_start, 'end': poly_end}}
@@ -86,6 +95,7 @@ def init_db(filenames):
                 records[stock_name][poly_name] = {'chr': poly_chr, 'orientation': orientation, 'start': poly_start,
                                                   'end': poly_end}
     return records
+
 
 def load_conf(conf_file='primer3.conf'):
     """
@@ -121,23 +131,25 @@ def get_seq(poly_entry, sequence_length_defs, genome):
     """
     :param poly_entry: dict item
     'SALK_001127.52.15.x': {'chr': '1', 'orientation': 'W', 'start': '32', 'end': '303'}
+    :param sequence_length_defs: dict
+    :param genome: pyfaidx.Fasta
     :return seq: DNA sequence as string
     """
     # calculate the upstream and downstream distances based on the sequence input params
-    bp_upstream = sequence_length_defs['maxN'] + sequence_length_defs['ext5'] + sequence_length_defs['p_zone']
-    bp_downstream = sequence_length_defs['ext3'] + sequence_length_defs['p_zone']
+    bp_upstream = sequence_length_defs['maxN'] + sequence_length_defs['ext5'] + int(sequence_length_defs['p_zone'] / 2)
+    bp_downstream = sequence_length_defs['ext3'] + int(sequence_length_defs['p_zone'] / 2)
     total_bp = bp_upstream + bp_downstream
     chrom = 'chr' + poly_entry['chr']
 
     # logic to account for orientation of T-DNA insert
     if poly_entry['orientation'] == 'W':
-        # need to handle overflow cases, should define chromsizes!
         new_start = max(1, int(poly_entry['start']) - bp_upstream)
         new_end = max(min(int(poly_entry['start']) + bp_downstream + 1, len(genome[chrom])), total_bp + new_start)
         seq = genome[chrom][new_start:new_end]
     elif poly_entry['orientation'] == 'C':
-        new_start = max(1, int(poly_entry['end']) - bp_downstream)
-        new_end = max(min(int(poly_entry['end']) + bp_upstream + 1, len(genome[chrom])), new_start + total_bp)
+        print("reverse")
+        new_start = max(int(poly_entry['end']) - bp_downstream, 1)
+        new_end = max(min(int(poly_entry['end']) + bp_upstream, len(genome[chrom])), new_start + total_bp)
         seq = -genome[chrom][new_start:new_end]
     else:
         raise ValueError("Orientation must be 'W' or 'C'!")
@@ -158,12 +170,13 @@ def make_primers(sequence, sequence_length_defs, primer3_seq_args, primer3_prime
     BPos=110 (distance from LB primer to insertion site)
     '''
     total = reduce(lambda x, y: x + y, [v for v in sequence_length_defs.values()])
-    primer3_seq_args['p_zone'] = [0, int(sequence_length_defs['p_zone'] / 2),
-                                  total - int((sequence_length_defs['p_zone'] / 2)), total]
-
+    primer3_seq_args['SEQUENCE_PRIMER_PAIR_OK_REGION_LIST'] = [0, int(sequence_length_defs['p_zone'] / 2),
+                                                               total - int((sequence_length_defs['p_zone'] / 2)), int((sequence_length_defs['p_zone'] / 2))]
+    primer3_primer_args['PRIMER_PRODUCT_SIZE_RANGE'] = [total - sequence_length_defs['p_zone'], total]
     primer3_seq_args['SEQUENCE_TEMPLATE'] = sequence
     a = designPrimers(primer3_seq_args, primer3_primer_args)
     return a
+
 
 if __name__ == '__main__':
     run_tdna_primers()
